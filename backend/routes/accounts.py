73a657473712eee744b5035e7ceb6fae6cc5652a
Mithun Surve <mithunsurve148@gmail.com>
2024-12-07 20:59:29 -0800

    Update accounts.py
    added balance check

--- backend/routes/accounts.py
+++ backend/routes/accounts.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, request, jsonify, session
 from services.account_creation import AccountService
 from models import db, User
+
 accounts_bp = Blueprint('accounts', __name__)
 
 
 @accounts_bp.route('/checking', methods=['GET'])
-def check_savings(username):
+def checking_balance(username):
     try:
         result, status_code = AccountService.checking_account_balance(username, "checking")
+        return jsonify(result), status_code
+
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
+
+
+@accounts_bp.route('/saving', methods=['GET'])
+def savings_balance(username):
+    try:
+        result, status_code = AccountService.checking_account_balance(username, "saving")
         return jsonify(result), status_code
 
     except Exception as e:
